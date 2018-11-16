c33d0c193b1533dec4005d993f3a73ed2cbfef1e
James <tytechortz@gmail.com>
2018-11-15 20:00:23 -0700

    first commit

---
+++ state-capitals.py
@@ -0,0 +1,12 @@
+state_capitals = {
+    "Colorado": "Denver",
+    "Arizona": "Phoenix",
+    "Utah": "Salt Lake City"
+}
+
+state_capitals["New Mexico"] = "Santa Fe"
+
+capital = raw_input("What is the Capital of " + state_capitals
+
+
+print (state_capitals['Colorado'])
